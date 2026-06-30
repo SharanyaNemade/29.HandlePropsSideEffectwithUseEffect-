0b5bda7ff1b11e41a006dd0eab7818cace7456e9
SharanyaNemade <sharanyanemade123@gmail.com>
2026-06-30 20:27:39 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
 import { useState } from "react";
 import "./App.css";
-import ReuseComponent from "./ReuseComponent";
+import Counter from "./Counter";
 
 function App() {
 
-  const userData = [
-  { id: 1, name: "John", age: 25, email: "john@example.com" },
-  { id: 2, name: "Jane", age: 30, email: "jane@example.com" },
-  { id: 3, name: "Alice", age: 28, email: "alice@example.com" },
-  { id: 4, name: "Bob", age: 35, email: "bob@example.com" }
-];
-
+  const [count, setCount] = useState(0);
+  const [data, setdata] = useState(0);
 
   return (
     <div>
-      <h1>Reuse Component In Loop</h1>
-      {
-         userData.map((user) => (
-          <ReuseComponent key={user.id}
-              user={user}/>
-        ))
-      }
+      <Counter count = {count} data = {data} />
+      
+      <button onClick={() => setCount(count+1)}>Counter {count}</button>
+
+      <button onClick={() => setdata(data+1)}>Data {data}</button>
     </div>
   );
 }
